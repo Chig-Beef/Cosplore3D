275c815f85a7c7514168e7ec84c202188e56a0c6
Chig Beef <rhystobyharris@gmail.com>
2024-02-13 11:42:51 +1030

    Created TextBox struct

---
+++ levelEditor/TextBox.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"image/color"
+	"strconv"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text"
+)
+
+type TextBox struct {
+	text      string
+	value     uint8
+	x         float64
+	y         float64
+	w         float64
+	h         float64
+	bgColor   color.Color
+	textcolor color.Color
+	active    bool
+}
+
+func (tb *TextBox) correct() {
+	n, err := strconv.Atoi(tb.text)
+	if err != nil {
+		n = 0
+	}
+	if n < 0 || n > 255 {
+		n = 0
+	}
+	tb.value = uint8(n)
+}
+
+func (tb *TextBox) draw(screen *ebiten.Image, g *Game) {
+	ebitenutil.DrawRect(screen, tb.x, tb.y, tb.w, tb.h, tb.bgColor)
+
+	text.Draw(screen, tb.text, g.fonts["colors"], int(tb.x), int(tb.y), tb.textcolor)
+
+	if tb.active {
+		ebitenutil.DrawLine(screen, tb.x, tb.y+tb.h, tb.x+tb.w, tb.y+tb.h, color.White)
+	}
+}
+
+func (tb *TextBox) check_click(g *Game) {
+	x := g.curMousePos[0]
+	y := g.curMousePos[1]
+
+	if int(tb.x) <= x && x <= int(tb.x+tb.w) {
+		if int(tb.y) <= y && y <= int(tb.y+tb.y) {
+			if tb.active {
+				tb.correct()
+			}
+			tb.active = !tb.active
+		}
+	}
+}
+
+func (tb *TextBox) update() {
+	if !tb.active {
+		return
+	}
+
+	for i := 43; i < 53; i++ {
+		if inpututil.IsKeyJustPressed(ebiten.Key(i)) {
+			tb.text += strconv.Itoa(i - 43)
+			return
+		}
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
+		if len(tb.text) > 0 {
+			tb.text = tb.text[:len(tb.text)-1]
+		}
+	}
+}
