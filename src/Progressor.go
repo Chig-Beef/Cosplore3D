daaf67727ece35b8e79760291a6b02e8a37e3c05
Chig Beef <rhystobyharris@gmail.com>
2024-02-13 16:08:44 +1030

    Added Progressors

---
+++ src/Progressor.go
@@ -0,0 +1,31 @@
+package main
+
+import "slices"
+
+type Progressor struct {
+	x         float64
+	y         float64
+	w         float64
+	h         float64
+	check     Check
+	levelName string
+}
+
+type Check func(*Game) bool
+
+func (p *Progressor) check_collide(g *Game) {
+	x := g.player.x
+	y := g.player.y
+
+	if p.x <= x && x <= p.x+p.w {
+		if p.y <= y && y <= p.y+p.h {
+			if p.check(g) {
+				g.player.curLevel = p.levelName
+			}
+		}
+	}
+}
+
+func has_cosmium(g *Game) bool {
+	return slices.Contains(g.player.inventory, "Cosmium")
+}
